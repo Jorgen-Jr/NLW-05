1d23154151bf2ff39a1fe315c221ae9171045e59
Jorgen-Jr <jorgenjr@outlook.com>
2021-04-22 01:16:04 -0300

    Day 3

---
+++ src/controllers/MessageController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { MessageService } from "../services/MessageService";
+
+class MessageController {
+  async create(req: Request, res: Response): Promise<Response> {
+    const { admin_id, user_id, text } = req.body;
+
+    const messageService = new MessageService();
+
+    const message = await messageService.create({ admin_id, user_id, text });
+
+    return res.json(message);
+  }
+
+  async showByUser(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const messageService = new MessageService();
+
+    const list = await messageService.listByUser(id);
+
+    return res.json(list);
+  }
+}
+
+export { MessageController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { MessageController } from "./controllers/MessageController";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messageController = new MessageController();
 
 const routes = Router();
 
 routes.post("/settings", settingsController.create);
+routes.post("/user", usersController.create);
+routes.post("/message", messageController.create);
+routes.get("/message/:id", messageController.showByUser);
 
 export { routes };
